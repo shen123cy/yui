c27cd51125484c4b1f05377b2e6ffe4b84ed9825
Kahle <Kahlkn@gmail.com>
2018-03-25 10:47:54 +0800

    Add some code generator

---
+++ yui-generator/src/main/java/com/github/kahlkn/yui/generator/Generator.java
@@ -0,0 +1,11 @@
+package com.github.kahlkn.yui.generator;
+
+/**
+ * Code generator interface.
+ * @author Kahle
+ */
+public interface Generator {
+
+    void generate() throws GenerateException;
+
+}
---
+++ yui-generator/src/main/java/com/github/kahlkn/yui/generator/apidoc/UrlMapping.java
@@ -0,0 +1,13 @@
+package com.github.kahlkn.yui.generator.apidoc;
+
+import com.github.kahlkn.yui.generator.AbstractGenerator;
+import com.github.kahlkn.yui.generator.GenerateException;
+
+public class UrlMapping extends AbstractGenerator {
+
+    @Override
+    public void generate() throws GenerateException {
+
+    }
+
+}
